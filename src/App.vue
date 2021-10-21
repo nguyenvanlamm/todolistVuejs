c78766190d6ba1f81d0d5b4bd8524068ced4ba63
nguyenvanlamm <lamnv260398@gmai.com>
2021-10-21 15:37:58 +0700

    Create todolist Vuejs

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>{{ tongtien | danhDau }}</span>
+    <button @click="tongtien+=20000">Cong tien</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      tongtien: 100000
+    }
+  },
+  filters: {
+    danhDau: function (tongtien) {
+      return tongtien.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+    }
+  }
+}
+
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
