a7013dce9b2fc7b981b7714b296d57e4192ad689
nguyenvanlamm <lamnv260398@gmai.com>
2021-10-22 10:27:50 +0700

    Done clear completed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,104 @@
 <template>
-  <div>
-    <span>{{ tongtien | danhDau }}</span>
-    <button @click="tongtien+=20000">Cong tien</button>
+  <div id="app">
+    <div>
+      <button @click="checkAll()">Tich all</button>
+      <input type="text" v-model="textTask" @keypress.enter="addTask()">
+    </div>
+
+    <div v-show="show" v-for="(task, index) in tasks" :key="index">
+      <input type="checkbox" v-model="task.done" @click="clickTaskToDo(task)">
+      <span :class="{done: task.done}">{{ task.content }}</span>
+      <button @click="Delete(index)">Delete</button>
+    </div>
+    <div>
+      {{ countToDo }} items left
+      <button>All</button>
+      <button @click="Active()">Active</button>
+      <button>Completed</button>
+      <button @click="ClearTaskCompleted()">Clear Completed</button>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+
   data() {
     return {
-      tongtien: 100000
+      currentToDo: null,
+      show: true,
+      countToDo: 5,
+      textTask: '',
+      tasks: [
+        {content: 'di cho', done: false},
+        {content: 'nau an', done: false},
+        {content: 'mua sam', done: false},
+        {content: 'chay the duc', done: false},
+        {content: 'xem phim', done: false},
+      ],
     }
   },
-  filters: {
-    danhDau: function (tongtien) {
-      return tongtien.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+
+  methods: {
+    loadCountToDo: function () {
+      let count = 0;
+      for (let i = 0; i < this.tasks.length; i++) {
+        if (!this.tasks[i].done) {
+          count++
+        }
+      }
+      this.countToDo = count;
+
+    },
+    clickTaskToDo: function (task) {
+      task.done = !task.done;
+      this.loadCountToDo();
+    },
+
+    checkAll: function () {
+      let tmp = false;
+      for (let i = 1; i < this.tasks.length; i++) {
+        if (this.tasks[i - 1].done != this.tasks[i].done) {
+          tmp = true;
+          break;
+        }
+      }
+
+      if (tmp == true) {
+        this.tasks.forEach((task) => {
+          if (!task.done) {
+            task.done = true
+          }
+        })
+      } else {
+        this.tasks.forEach(task => task.done = !task.done)
+      }
+      this.loadCountToDo();
+    },
+
+    addTask: function () {
+      this.tasks.push({content: this.textTask, done: false})
+      this.textTask = ''
+      this.loadCountToDo()
+    },
+
+    Delete: function (index) {
+      this.tasks.splice(index, 1);
+      this.loadCountToDo();
+    },
+
+    Active: function () {
+
+    },
+    ClearTaskCompleted: function () {
+      let newTasks = [];
+      this.tasks.forEach(item => {
+        if (!item.done) {
+          newTasks.push(item)
+        }
+      })
+      return this.tasks = newTasks;
     }
   }
 }
